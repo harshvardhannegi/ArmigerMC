15ec04086c970150fd90944dca9ee39ad7b11fb2
doflamingonegi <harshvardhannegi@yandex.com>
2026-01-31 14:29:56 +0530

    Added: Bow Of Raven with Weapons Class.

--- src/main/java/me/negi/armigerMC/Weapons/Weapons/BowOfRaven.java
+++ src/main/java/me/negi/armigerMC/Weapons/Weapons/BowOfRaven.java
@@ -1,4 +1,23 @@
 package me.negi.armigerMC.Weapons.Weapons;
 
-public class BowOfRaven {
+import me.negi.armigerMC.Weapons.Weapon;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class BowOfRaven implements Weapon {
+    @Override
+    public void onExecution(Player player, Entity entity) {
+
+    }
+
+    @Override
+    public ItemStack getWeaponItem() {
+        return null;
+    }
+
+    @Override
+    public int getMANACost() {
+        return 0;
+    }
 }
